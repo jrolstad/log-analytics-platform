73f93bf284743402094c3bc45ddec64b878523f8
Josh Rolstad <jrolstad@gmail.com>
2023-12-24 17:14:39 -0800

    Fixing name issue

--- deployments/infra/functions.tf
+++ deployments/infra/functions.tf
@@ -1,6 +1,6 @@
 resource "oci_functions_application" "primary" {
   compartment_id = oci_identity_compartment.primary.id
   display_name   = "${var.service_name}-${var.environment}"
-  subnet_ids     = [oci_core_subnet.subnet_1.id]
+  subnet_ids     = [oci_core_subnet.primary.id]
   freeform_tags  = { "service" = var.service_name, "environment" = var.environment }
 }
