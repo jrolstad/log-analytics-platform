df2513aa05eaba267c4f1d609f810672708f886a
Josh Rolstad <jrolstad@gmail.com>
2023-12-24 16:15:35 -0800

    Stubbing out terraform to build the infrastructure

---
+++ deployments/infra/oci.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "5.10.0"
+    }
+  }
+  backend "http" {
+    update_method = "PUT"
+  }
+}
+
+provider "oci" {
+  tenancy_ocid     = var.tenant_id
+  user_ocid        = var.user_id
+  private_key_path = var.auth_keyfile
+  fingerprint      = var.auth_fingerprint
+  region           = var.region
+}
---
+++ deployments/infra/variables.tf
@@ -0,0 +1,40 @@
+variable "tenant_id" {
+  description = "Tenant the resources are associated with"
+  type        = string
+}
+
+variable "root_compartment_id" {
+  description = "Root Compartment Id for the tenancy"
+  type        = string
+}
+
+variable "user_id" {
+  description = "User the IaC process is running as"
+  type        = string
+}
+
+variable "region" {
+  description = "OCI region to deploy resources to"
+  type        = string
+}
+
+variable "auth_fingerprint" {
+  description = "Fingerprint of the certificate the user authenticates as"
+  type        = string
+}
+
+variable "auth_keyfile" {
+  description = "Path to the keyfile the user authenticates with"
+  type        = string
+}
+
+variable "service_name" {
+  description = "Name of the service"
+  type        = string
+  default     = "log-analyzer"
+}
+
+variable "environment" {
+  description = "What type of environment (dev,tst,prd)"
+  type        = string
+}
