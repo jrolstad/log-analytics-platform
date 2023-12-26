f8944319daa756acbbbdfa85883c5de5d3cec4a9
Josh Rolstad <jrolstad@gmail.com>
2023-12-26 10:55:11 -0800

    Adding data

--- deployments/infra/functions.tf
+++ deployments/infra/functions.tf
@@ -14,7 +14,7 @@ data "oci_artifacts_container_images" "file_receiver" {
 resource "oci_functions_function" "file_published_handler" {
   application_id = oci_functions_application.primary.id
   display_name   = "file-published-handler"
-  image = oci_artifacts_container_images.file_receiver[0].id
+  image = data.oci_artifacts_container_images.file_receiver[0].id
   memory_in_mbs = 2048
   #config = var.function_config
   timeout_in_seconds = 300
