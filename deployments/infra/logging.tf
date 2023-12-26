4dd18dc81297590bc46742f116e3c6969de1a865
Josh Rolstad <jrolstad@gmail.com>
2023-12-26 13:17:44 -0800

    trying different values

--- deployments/infra/logging.tf
+++ deployments/infra/logging.tf
@@ -5,16 +5,16 @@ resource "oci_logging_log_group" "primary" {
 }
 
 resource "oci_logging_log" "connector" {
-  display_name = "connectorhub"
+  display_name = "connectorhub-runlog"
   log_group_id = oci_logging_log_group.primary.id
   log_type     = "SERVICE"
 
   configuration {
     compartment_id = oci_identity_compartment.primary.id
     source {
-      category    = "all"
+      category    = "runlog"
       resource    = oci_sch_service_connector.file_published_handler.id
-      service     = "Connector Hub"
+      service     = "sch.serviceconnector"
       source_type = "OCISERVICE"
     }
   }
