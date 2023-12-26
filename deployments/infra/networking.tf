ab70b5d6e659e300e264cbbdc1172a9c9055f387
Josh Rolstad <jrolstad@gmail.com>
2023-12-26 13:44:41 -0800

    Enabling for all services

--- deployments/infra/networking.tf
+++ deployments/infra/networking.tf
@@ -11,3 +11,18 @@ resource "oci_core_subnet" "primary" {
   cidr_block     = "10.0.0.0/24"
   freeform_tags  = { "service" = var.service_name, "environment" = var.environment }
 }
+
+data "oci_core_services" "all" {
+}
+
+resource "oci_core_service_gateway" "primary" {
+  compartment_id = oci_identity_compartment.primary.id
+  vcn_id         = oci_core_virtual_network.primary.id
+  dynamic "services" {
+    for_each = data.oci_core_services.all
+    content {
+      service_id = each.id
+    }
+  }
+  freeform_tags = { "service" = var.service_name, "environment" = var.environment }
+}
