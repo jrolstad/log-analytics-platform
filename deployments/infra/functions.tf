9a78863acaf94d4c6651e6305f3ab25bfe89b8a3
Josh Rolstad <jrolstad@gmail.com>
2023-12-26 08:08:42 -0800

    Removing function for now

--- deployments/infra/functions.tf
+++ deployments/infra/functions.tf
@@ -5,12 +5,12 @@ resource "oci_functions_application" "primary" {
   freeform_tags  = { "service" = var.service_name, "environment" = var.environment }
 }
 
-resource "oci_functions_function" "file_published_handler" {
-  application_id = oci_functions_application.primary.id
-  display_name   = "file-published-handler"
-  #image = var.function_image
-  memory_in_mbs = 2048
-  #config = var.function_config
-  timeout_in_seconds = 300
-  freeform_tags      = { "service" = var.service_name, "environment" = var.environment }
-}
+#resource "oci_functions_function" "file_published_handler" {
+#  application_id = oci_functions_application.primary.id
+#  display_name   = "file-published-handler"
+#  image = var.function_image
+#  memory_in_mbs = 2048
+#  #config = var.function_config
+#  timeout_in_seconds = 300
+#  freeform_tags      = { "service" = var.service_name, "environment" = var.environment }
+#}
