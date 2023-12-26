3833e6fc40547f73b3b11643a2ec158fe8bc27b4
Josh Rolstad <jrolstad@gmail.com>
2023-12-26 14:11:12 -0800

    Adding function logging

--- deployments/infra/logging.tf
+++ deployments/infra/logging.tf
@@ -22,3 +22,22 @@ resource "oci_logging_log" "connector" {
   is_enabled         = true
   retention_duration = 30
 }
+
+resource "oci_logging_log" "functions" {
+  display_name = "functions-invoke"
+  log_group_id = oci_logging_log_group.primary.id
+  log_type     = "SERVICE"
+
+  configuration {
+    compartment_id = oci_identity_compartment.primary.id
+    source {
+      category    = "invoke"
+      resource    = oci_functions_application.primary.id
+      service     = "functions"
+      source_type = "OCISERVICE"
+    }
+  }
+  freeform_tags      = { "service" = var.service_name, "environment" = var.environment }
+  is_enabled         = true
+  retention_duration = 30
+}
