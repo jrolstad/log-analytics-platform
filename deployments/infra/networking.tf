7976f7e2d3cb693483eb4b803a984c63ed3a347b
Josh Rolstad <jrolstad@gmail.com>
2024-01-14 11:19:27 -0800

    Adding extra network objects from sample app

--- deployments/infra/networking.tf
+++ deployments/infra/networking.tf
@@ -5,11 +5,47 @@ resource "oci_core_virtual_network" "primary" {
   freeform_tags  = { "service" = var.service_name, "environment" = var.environment }
 }
 
-resource "oci_core_subnet" "primary" {
+resource "oci_core_internet_gateway" "primary" {
   compartment_id = oci_identity_compartment.primary.id
+  display_name   = "${var.service_name}-${var.environment}"
   vcn_id         = oci_core_virtual_network.primary.id
-  cidr_block     = "10.0.0.0/24"
-  freeform_tags  = { "service" = var.service_name, "environment" = var.environment }
+}
+
+resource "oci_core_route_table" "primary" {
+  compartment_id = oci_identity_compartment.primary.id
+  display_name   = "${var.service_name}-${var.environment}"
+  vcn_id         = oci_core_virtual_network.primary.id
+  route_rules {
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+    network_entity_id = oci_core_internet_gateway.primary.id
+  }
+}
+
+resource "oci_core_dhcp_options" "primary" {
+  compartment_id = oci_identity_compartment.primary.id
+  display_name   = "${var.service_name}-${var.environment}"
+  vcn_id         = oci_core_virtual_network.primary.id
+
+  // required
+  options {
+    type        = "DomainNameServer"
+    server_type = "VcnLocalPlusInternet"
+  }
+
+}
+
+resource "oci_core_subnet" "primary" {
+  compartment_id    = oci_identity_compartment.primary.id
+  display_name      = "${var.service_name}-${var.environment}"
+  vcn_id            = oci_core_virtual_network.primary.id
+  cidr_block        = "10.0.0.0/24"
+  dns_label         = "fnsub"
+  route_table_id    = oci_core_route_table.primary.id
+  dhcp_options_id   = oci_core_dhcp_options.primary.id
+  security_list_ids = [oci_core_virtual_network.primary.default_security_list_id]
+
+  freeform_tags = { "service" = var.service_name, "environment" = var.environment }
 }
 
 data "oci_core_services" "all" {
